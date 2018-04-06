c196abdab1f30859cf40a8ed99d183fe6e606d57
DJ Jahn <dj.jahn@aviture.us.com>
2018-04-06 08:33:35 -0500

    Mapping to SCSS vs. CSS pages / Hello World

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div class="test">123</div>
-        <div class="test second">2<span>3</span>4</div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (<div className="App">Hello World!</div>);
+};
 
 export default App;
