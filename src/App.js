0474776a7f179eda8b563800274aadb25a7e5fda
DJ Jahn <dj.jahn@aviture.us.com>
2018-04-06 09:13:34 -0500

    renamed containers to components... for later reuse

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
-import HelloWorld from './containers/HelloWorld/HelloWorld';
+import HelloWorld from './components/HelloWorld/HelloWorld';
 
 const App = () => {
   return (
     <div className="App">
+      <HelloWorld/>
+      <HelloWorld/>
       <HelloWorld/>
     </div>);
 };
