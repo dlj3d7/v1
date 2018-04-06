94f293e4ab10660cb1397d15b16784ac1dac3d95
DJ Jahn <dj.jahn@aviture.us.com>
2018-04-06 09:05:02 -0500

    Adding 2nd Page & Containers Section for Views

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.scss';
+import HelloWorld from './containers/HelloWorld/HelloWorld';
 
 const App = () => {
-  return (<div className="App">Hello World!</div>);
+  return (
+    <div className="App">
+      <HelloWorld/>
+    </div>);
 };
 
 export default App;
---
+++ src/containers/HelloWorld/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HelloWorld.scss';
+
+const HelloWorld = () => {
+	return (<div className="HelloWorld">Hello World!</div>);
+};
+
+export default HelloWorld;
